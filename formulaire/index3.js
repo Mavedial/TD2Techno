ff3dc43fdc3c1ba5ba81f7d1322857e9b15e4450
Mathis WARCHE <mathixx59@outlook.fr>
2025-09-25 23:14:04 +0200

    Exercice sur les choix (iii)

--- formulaire/index3.js
+++ formulaire/index3.js
@@ -22,12 +22,47 @@ rd2.textContent = "Casque";
 rd3.textContent = "Bluetooth";
 
 // (4)
-const choix = document.querySelectorAll(input[name="choix"]);
+const choix = document.querySelectorAll('input[name="choix"]');
+const namevolume = document.querySelector('label[for="volume"]');
 
 choix.forEach(choix =>{
     choix.addEventListener('change', function() {
         if (this.value ==="1"){
-            volume.textContent="caojnjaojc";
+            namevolume.textContent="Volume HP";
+        }
+        if (this.value ==="2"){
+            namevolume.textContent="Volume Casque";
+        }
+        if (this.value==="3"){
+            namevolume.textContent="Volume Bluetooth";
         }
     })
 })
+
+const volume =document.getElementById('volume');
+volume.max=100;
+console.log(volume.max);
+
+const valeurVolume = document.getElementById('value_volume');
+
+function updateValue(slide){
+    valeurVolume.textContent = slide.value;
+}
+
+const descriptionCase =document.querySelector('label[for="checkbox1"]');
+descriptionCase.textContent = "Mute";
+
+const caseCheck =document.getElementById('checkbox1');
+
+caseCheck.oninput =function(){
+    if(this.checked){
+        valeurVolume.textContent= "X";
+        volume.disabled= true;
+    }
+    else{
+        volume.disabled=false;
+        valeurVolume.textContent= volume.value;
+    }
+}
+
+// (5)
